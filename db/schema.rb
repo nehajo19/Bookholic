8a9fd0f2653b7915822b8a433a731c2234a6c8b5
Rucha Kulkarni <rucha.g.kulkarni@gmail.com>
2020-01-25 17:56:59 +0530

    book device generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_105919) do
+ActiveRecord::Schema.define(version: 2020_01_25_113729) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "subject"
+    t.string "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
